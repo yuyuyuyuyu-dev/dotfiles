548107bde23412c0a6b42e6b5fe210af92f87528
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2020-02-10 19:33:26 +0900

    プロンプトを、正常終了のときは現在時刻とユーザー名、異常終了のときはプラズマちゃんにした

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,29 +1,44 @@
 function fish_prompt --description 'Write out the prompt'
-    if [ {$status} -ne 0 ]
-      echo '　　　 _,／￣￣`￣＼､/ﾚ'
-      echo '　　／/ 　 ,　 /＼　.i i Ｖ〈'
-      echo '　 / /　 ∠ﾑ/ ー-V l　「ヽ'
-      echo '　　 j v､!●　　● i　'　├''
-      echo '　／　 〈　　 ワ　 /　.i y\''
-      echo '/ _ ,.イ , `ｰｩ　 ｔ-!,､_У'
-      echo '´　\'　.ﾚ^V´　Ｖ_,ィtｰ〈　　｢| ｢|'
-      echo '　　　　 /　`央ー\'j　　＼_|:| |:|'
-      echo '　　　 ,/ー､{,_ﾉ　/ー､! 　＼::::]'
-    end
+  set exit_status {$status}
 
-    # Host
-    echo
-    set_color $fish_color_host
-    echo -n (prompt_hostname)
-    set_color normal
+  set_color $fish_color_user
 
-    echo -n ': '
+  echo
 
-    # User
-    set_color $fish_color_user
+  if [ {$exit_status} -ne 0 ]
+    echo '       _,／￣￣`￣＼､/ﾚ'
+    echo '    ／/    ,   /＼  .i i Ｖ〈'
+    echo '   / /   ∠ﾑ/ ー-V l  「ヽ'
+    echo '     j v､!●    ● i  '  ├''
+    echo '  ／   〈     ワ   /  .i y\''
+    echo '/ _ ,.イ , `ｰｩ   ｔ-!,､_У'
+    echo '´  \'  .ﾚ^V´  Ｖ_,ィtｰ〈    ｢| ｢|'
+    echo '         /  `央ー\'j    ＼_|:| |:|'
+    echo -n '       ,/ー､{,_ﾉ  /ー､!   ＼::::]'
+  else
+    echo -n ' '
+    echo -n (date '+%H')
+    echo -n ':'
+    echo -n (date '+%M')
+    echo -n ' '
     echo -n (whoami)
-    set_color normal
+  end
 
-    echo -n '$ '
-    set_color normal
+  echo -n '$ '
+set_color normal
+
+  # Host
+  #   set_color $fish_color_host
+  #   echo -n (prompt_hostname)
+  #   set_color normal
+
+  # echo -n ': '
+
+  # User
+  # set_color $fish_color_user
+  # echo -n (whoami)
+  # set_color normal
+
+  # echo -n '$ '
+  # set_color normal
 end
