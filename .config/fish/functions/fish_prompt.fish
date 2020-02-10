1842fc20b3e1617fdac6acb507e8839f98da1ae0
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2020-02-10 19:04:25 +0900

    直前のコマンドの終了ステータスが0以外だったらプラズマちゃんを召喚するようにした

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,7 +1,18 @@
 function fish_prompt --description 'Write out the prompt'
-    echo
+    if [ {$status} -ne 0 ]
+      echo '　　　 _,／￣￣`￣＼､/ﾚ'
+      echo '　　／/ 　 ,　 /＼　.i i Ｖ〈'
+      echo '　 / /　 ∠ﾑ/ ー-V l　「ヽ'
+      echo '　　 j v､!●　　● i　'　├''
+      echo '　／　 〈　　 ワ　 /　.i y\''
+      echo '/ _ ,.イ , `ｰｩ　 ｔ-!,､_У'
+      echo '´　\'　.ﾚ^V´　Ｖ_,ィtｰ〈　　｢| ｢|'
+      echo '　　　　 /　`央ー\'j　　＼_|:| |:|'
+      echo '　　　 ,/ー､{,_ﾉ　/ー､! 　＼::::]'
+    end
 
     # Host
+    echo
     set_color $fish_color_host
     echo -n (prompt_hostname)
     set_color normal
