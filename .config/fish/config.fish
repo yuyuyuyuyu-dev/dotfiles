1592d8c8df47968b0a8667576a0a40646fd02902
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2021-06-27 12:04:50 +0900

    ホームディレクトリにmyCommandsディレクトリが無かったら作るようにした

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,72 +1,85 @@
 # 文字コードをUTF-8にする
 set -x LC_ALL ja_JP.UTF-8
 
+
 # パスの設定
-if [ -d {$HOME}/myCommands ]
-  set -x PATH {$PATH} {$HOME}/myCommands
+if ! [ -e {$HOME}/myCommands ]
+    mkdir {$HOME}/myCommands
 end
+set -x PATH {$PATH} {$HOME}/myCommands
+
 if [ -d {$HOME}/.sdkman/candidates/java/current/bin ]
-  set -x PATH {$PATH} {$HOME}/.sdkman/candidates/java/current/bin
+    set -x PATH {$PATH} {$HOME}/.sdkman/candidates/java/current/bin
 end
+
 if [ -d {$HOME}/.sdkman/candidates/kotlin/current/bin ]
-  set -x PATH {$PATH} {$HOME}/.sdkman/candidates/kotlin/current/bin
+    set -x PATH {$PATH} {$HOME}/.sdkman/candidates/kotlin/current/bin
 end
+
 if [ -d {$HOME}/.local/bin ]
-  set -x PATH {$PATH} {$HOME}/.local/bin
+    set -x PATH {$PATH} {$HOME}/.local/bin
 end
+
 if [ -d {$HOME}/.cargo/bin ]
     set -x PATH {$PATH} {$HOME}/.cargo/bin
 end
+
 if [ -d {$HOME}/.nimble/bin ]
     set -x PATH {$PATH} {$HOME}/.nimble/bin
 end
 
+
 # prompt_pwdでパスを省略しない
 set -g fish_prompt_pwd_dir_length 0
+
 
 # neovimに必要な設定
 set -x XDG_CONFIG_HOME {$HOME}/.config
 set -x XDG_CACHE_HOME {$HOME}/.cache
 
+
 # デフォルトのエディタの設定
 # neovim、vim、viの順番でインストールされているか確認して、されていたらそれに設定する
 if type nvim > /dev/null 2>&1
-  set -x EDITOR nvim
-  set -x VISUAL nvim
+    set -x EDITOR nvim
+    set -x VISUAL nvim
 else if type vim > /dev/null 2>&1
-  set -x EDITOR vim
-  set -x VISUAL vim
+    set -x EDITOR vim
+    set -x VISUAL vim
 else if type vi > /dev/null 2>&1
-  set -x EDITOR vi
-  set -x VISUAL vi
+    set -x EDITOR vi
+    set -x VISUAL vi
 else
-  echo 'nvimもvimもviもありませんでした'
+    echo 'nvimもvimもviもありませんでした'
 end
+
 
 # HomebrewのAnalyticsを停止
 set -x HOMEBREW_NO_ANALYTICS 1
+
 
 # エイリアスの設定
 alias less "less -cmN"
 alias del "delete"
 
+
 # OS毎の設定
 switch (uname) # なぜかlinterに引っかかる
-  case Darwin # Macのとき
-    alias safari "open -a /Applications/Safari.app"
-    alias chrome "open -a /Applications/Google\ Chrome.app"
-    alias firefox "open -a /Applications/Firefox.app"
-    alias ls "ls -FG"
-    alias la "ls -aFG"
-    alias ll "ls -FGl"
-    alias pushDotfiles "open -a /Applications/Safari.app https://www.github.com/yuu-kobayashi/dotfiles/ && cd {$HOME}/dotfiles/ && git add . && git commit && git push origin master"
-  case Linux # Linuxのとき
-    alias ls "ls -F --color=auto"
-    alias la "ls -aF --color=auto"
-    alias ll "ls -Fl --color=auto"
+    case Darwin # Macのとき
+        alias safari "open -a /Applications/Safari.app"
+        alias chrome "open -a /Applications/Google\ Chrome.app"
+        alias firefox "open -a /Applications/Firefox.app"
+        alias ls "ls -FG"
+        alias la "ls -aFG"
+        alias ll "ls -FGl"
+        alias pushDotfiles "open -a /Applications/Safari.app https://www.github.com/yuu-kobayashi/dotfiles/ && cd {$HOME}/dotfiles/ && git add . && git commit && git push origin master"
+    case Linux # Linuxのとき
+        alias ls "ls -F --color=auto"
+        alias la "ls -aF --color=auto"
+        alias ll "ls -Fl --color=auto"
 end
 
 # Windows Subsystem for Linuxのとき
 if uname -r | grep -i Microsoft > /dev/null 2>&1
-  alias firefox "/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
+    alias firefox "/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
 end
