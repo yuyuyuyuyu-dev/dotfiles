3efadf7bf92c4416def1974605a3351088abcd97
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-08-06 11:19:50 +0900

    Neovimの設定を変更しました。

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -9,16 +9,32 @@
 -- 参考資料
 -- https://neovim.io/doc/user/lua-guide.html#lua-guide
 
+-- 文字コードの設定
 -- set fileencodings=
 vim.o.fileencodings = "utf-8,cp932,sjis,ec-jp"
 
+-- 改行コードの設定
 -- set fileformats=
-vim.o.fileformats = "dos,unix,mac"
+vim.o.fileformats = vim.fn.has("win64") and "dos,unix,mac" or "unix,dos,mac"
 
--- rust-analyzerの設定が見つからない問題への対処
--- https://github.com/williamboman/mason.nvim/issues/1289#issuecomment-1550309536
--- だめだった
--- vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (vim.fn.has("win64") and ";" or ":") .. vim.env.PATH
+-- シェルを指定する（Windows環境用）
+if vim.fn.has("win64") then
+	vim.o.shell = string.gsub(vim.fn.system("powershell -c (gcm nu).Definition"), "\n", "")
+	vim.o.shellcmdflag = "-c nu --login"
+end
+
+-- マウスに反応しないようにする
+-- set mouse=
+vim.o.mouse = ""
+
+-- 左右のカーソル移動で行をまたがないようにする
+-- set whichwrap=
+vim.o.whichwrap = ""
+
+-- インサートモードでのbackspaceで改行とautoindentを削除できないようにする
+-- set backspace=
+vim.o.backspace = "start"
+
 
 -- 文字幅の問題はターミナルエミュレータのHyperに丸投げする
 -- vim.o.ambiwidth = "double"
