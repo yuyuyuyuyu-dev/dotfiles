e13a7bb7e5f2d72ddff2ce5e1d078ceb6eb21677
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-08-06 15:21:09 +0900

    入力切り替えのショートカットに反応しないようにしました。

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -4,11 +4,13 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-S-j>"] = { "<Nop>", "入力切り替えのショートカットに反応しないようにする", opts = { nowait = true } },
+    ["<C-S-;>"] = { "<Nop>", "入力切り替えのショートカットに反応しないようにする", opts = { nowait = true } },
   },
   i = {
     [";<ESC>"] = { "<Cmd>lua require('insert-end-semicolon').insertEndSemicolon()<CR><ESC>", "insert 「;」 at the end of line", opts = { nowait = true } },
-    ["<C-S-j>"] = { "<Nop>", "", opts = { nowait = true } },
-    ["<C-S-;>"] = { "<Nop>", "", opts = { nowait = true } },
+    ["<C-S-j>"] = { "<Nop>", "入力切り替えのショートカットに反応しないようにする", opts = { nowait = true } },
+    ["<C-S-;>"] = { "<Nop>", "入力切り替えのショートカットに反応しないようにする", opts = { nowait = true } },
   },
 }
 
