09ff4d6e872fdd425f2ea57ca22367c8f7c8c7f0
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-08-14 10:24:23 +0900

    Windows環境用にHyperの設定を追加しました。

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 mklink %homepath%\_vimrc %homepath%\dotfiles\.vimrc
 mklink /D %localappdata%\nvim\lua\custom %homepath%\dotfiles\nvchad\custom
+mklink %appdata%\Hyper\.hyper.js %homepath%\dotfiles\.hyper.js
 pause
