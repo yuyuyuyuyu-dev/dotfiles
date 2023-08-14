278fd2577fd14cfaf49037510660462a933991df
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-08-14 11:51:21 +0900

    空行を追加しました。

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,32 +1,50 @@
 function update 
   if type brew > /dev/null 2>&1
+    echo
+    echo
     echo '########'
     echo 'brew'
     echo '########'
     brew update -v && brew upgrade -v
   end
   if type apt-get > /dev/null 2>&1
+    echo
+    echo
     echo '########'
     echo 'apt'
     echo '########'
     sudo apt update && sudo apt upgrade
   end
   if type volta > /dev/null 2>&1
+    echo
+    echo
     echo '########'
     echo 'volta'
     echo '########'
     volta install node
   end
   if type rustup > /dev/null 2>&1
+    echo
+    echo
     echo '########'
     echo 'rustup'
     echo '########'
     rustup update
   end
-  if cargo update -h > /dev/null 2>&1
+  if cargo install-update --version > /dev/null 2>&1
+    echo
+    echo
     echo '########'
     echo 'cargo'
     echo '########'
-    cargo update -v
+    cargo install-update -a
+  end
+  if type pyenv > /dev/null 2>&1
+    echo
+    echo
+    echo '########'
+    echo 'pyenv'
+    echo '########'
+    pyenv update
   end
 end
