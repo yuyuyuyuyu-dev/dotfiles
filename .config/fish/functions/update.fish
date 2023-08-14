15ace0f65740a1c09338925015a81d79c139e19a
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-08-14 11:22:05 +0900

    一括でパッケージの更新をする関数を追加しました。

---
+++ .config/fish/functions/update.fish
@@ -0,0 +1,32 @@
+function update 
+  if type brew > /dev/null 2>&1
+    echo '########'
+    echo 'brew'
+    echo '########'
+    brew update -v && brew upgrade -v
+  end
+  if type apt-get > /dev/null 2>&1
+    echo '########'
+    echo 'apt'
+    echo '########'
+    sudo apt update && sudo apt upgrade
+  end
+  if type volta > /dev/null 2>&1
+    echo '########'
+    echo 'volta'
+    echo '########'
+    volta install node
+  end
+  if type rustup > /dev/null 2>&1
+    echo '########'
+    echo 'rustup'
+    echo '########'
+    rustup update
+  end
+  if cargo update -h > /dev/null 2>&1
+    echo '########'
+    echo 'rustup'
+    echo '########'
+    cargo update -v
+  end
+end
