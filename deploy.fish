983bb825ebf9ee58bd175903d0c3204d81bd7147
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-08-15 15:23:53 +0900

    fish_indentにかけました。

--- deploy.fish
+++ deploy.fish
@@ -5,41 +5,41 @@
 
 # リンクを貼るかどうかを判定する関数
 function ignore
-  # リンクを貼りたくないものはここに入れておく
-  set ignores '.' '..' '.DS_Store' '.git' '.gitignore' '.hyper.js'
+    # リンクを貼りたくないものはここに入れておく
+    set ignores '.' '..' '.DS_Store' '.git' '.gitignore' '.hyper.js'
 
-  for i in {$ignores}
-    if test {$i} = {$argv}
-      return 0
+    for i in {$ignores}
+        if test {$i} = {$argv}
+            return 0
+        end
     end
-  end
-  return 1
+    return 1
 end
 
 
 # 「.」から始まるファイルのリンクを貼る
 for file in .*
-  # リンクを貼りたくないファイルは無視する
-  if ignore {$file}
-    continue
-  end
+    # リンクを貼りたくないファイルは無視する
+    if ignore {$file}
+        continue
+    end
 
-  # リンク先にファイルが存在していたらリネームする
-  if test -e {$HOME}/{$file}
-    mv -h {$HOME}/{$file} {$HOME}/{$file}.bak
-  end
+    # リンク先にファイルが存在していたらリネームする
+    if test -e {$HOME}/{$file}
+        mv -h {$HOME}/{$file} {$HOME}/{$file}.bak
+    end
 
-  # リンクを貼る
-  ln -fns {$HOME}/dotfiles/{$file} {$HOME}/{$file}
+    # リンクを貼る
+    ln -fns {$HOME}/dotfiles/{$file} {$HOME}/{$file}
 end
 
 # NvChadの設定ファイル
 if test -e {$HOME}/.config/nvim/lua/custom
-  mv -h {$HOME}/.config/nvim/lua/custom {$HOME}/.config/nvim/lua/custom.bak
+    mv -h {$HOME}/.config/nvim/lua/custom {$HOME}/.config/nvim/lua/custom.bak
 end
 ln -fns {$HOME}/dotfiles/nvchad/custom {$HOME}/.config/nvim/lua/custom
 
 # gitの設定ファイル（端末固有なもの用）
 if ! test -e {$HOME}/gitconfig.local
-  cp gitconfig.local.template {$HOME}/.gitconfig.local
+    cp gitconfig.local.template {$HOME}/.gitconfig.local
 end
