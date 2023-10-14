dbaea0231e72d8250fe81da90b3a01c9fbc17e4f
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-10-14 15:59:38 +0900

    Windows環境でNYAGOSの設定ファイルもリンクを貼るようにしました。

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,6 @@
 mklink %homepath%\_vimrc %homepath%\dotfiles\.vimrc
 mklink /D %localappdata%\nvim\lua\custom %homepath%\dotfiles\nvchad\custom
 mklink %appdata%\Hyper\.hyper.js %homepath%\dotfiles\.hyper.js
+mklink %homepath%\_nyagos %homepath%\dotfiles\_nyagos
+mklink %homepath%\.nyagos %homepath%\dotfiles\.nyagos
 pause
