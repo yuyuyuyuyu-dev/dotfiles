76798c11d9966b4238e107dfc9ebfffc3c48681a
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-10-14 17:08:30 +0900

    DroidVimのmvコマンドに-hオプションが無いっぽいから削除

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 for i in .bash*; do
 	# すでにファイルが存在した場合は元のファイルをリネームする
 	if [ -e "${HOME}/${i}" ]; then
-		mv -h "${HOME}/${i}" "${HOME}/${i}.bak"
+		mv "${HOME}/${i}" "${HOME}/${i}.bak"
 	fi
 
 	# リンクを貼る
