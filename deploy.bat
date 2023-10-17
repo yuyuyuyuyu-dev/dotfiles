92a964d39361398a492b07ebfa2361fb12c78306
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-10-17 18:27:11 +0900

    [deploy.bat]コミットの署名に使う公開鍵を指定するようにしました

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,7 @@
 @echo off
 
+
+rem シンボリックリンクを貼る
 mklink %homepath%\_vimrc %homepath%\dotfiles\.vimrc
 mklink /D %homepath%\.config %homepath%\dotfiles\.config
 mklink /D %localappdata%\nvim\lua\custom %homepath%\dotfiles\nvchad\custom
@@ -7,7 +9,7 @@ mklink %appdata%\Hyper\.hyper.js %homepath%\dotfiles\.hyper.js
 mklink %homepath%\.nyagos %homepath%\dotfiles\.nyagos
 
 
-rem Gitの設定
+rem Gitの設定をする
 rem %homepath%\.gitconfigが存在しなかったら作成する
 rem このファイルが存在しなかった場合、`git config --global`で設定したときに%homepath%\.config\git\configに書き込まれてしまう
 if not exist %homepath%\.gitconfig type nul >> %homepath%\.gitconfig
@@ -16,5 +18,9 @@ rem 改行コード
 rem プッシュするときはLFに変換する
 rem プルするときはCRLFに変換する
 git config --global core.autocrlf true
+
+rem SSHの公開鍵を指定する
+git config --global user.signingkey %homepath%\.ssh\id_ed25519.pub
+
 
 pause
