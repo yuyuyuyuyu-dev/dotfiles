f5aca823b22cea1a609df573c36faf7d2f1e6887
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-10-18 18:19:19 +0900

    [deploy.bat]Gitのユーザー情報が設定されていなかった場合に対話形式で設定するようにしました

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal
+setlocal enabledelayedexpansion
 
 
 rem シンボリックリンクを貼る
@@ -13,6 +15,19 @@ rem Gitの設定をする
 rem %homepath%\.gitconfigが存在しなかったら作成する
 rem このファイルが存在しなかった場合、`git config --global`で設定したときに%homepath%\.config\git\configに書き込まれてしまう
 if not exist %homepath%\.gitconfig type nul >> %homepath%\.gitconfig
+
+rem ユーザー名が設定されてなかったら対話形式で設定する
+git config --global user.name > nul 2>&1
+if errorlevel 1 (
+  set /p name="ユーザー名に設定する文字列を入力してください> "
+  git config --global user.name !name!
+)
+rem メールアドレスが設定されてなかったら対話形式で設定する
+git config --global user.email > nul 2>&1
+if errorlevel 1 (
+  set /p email="ユーザーのメールアドレスに設定する文字列を入力してください> "
+  git config --global user.email !email!
+)
 
 rem 改行コード
 rem プッシュするときはLFに変換する
